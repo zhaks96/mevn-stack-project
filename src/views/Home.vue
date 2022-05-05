3cb21386a1cc1aaf79e7435ac8b2ade908214dda
zhaks96 <jansultan_141096@mail.ru>
2022-05-05 18:05:31 +0600

    mevn

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,40 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app id="inspire">
+    <v-app-bar app color="green" dark>
+      <v-app-bar-nav-icon @click.stop="mini = !mini" />
+      <v-toolbar-title style="width: 300px" class="ml-0 pl-4">
+        <span class="hidden-sm-and-down">MEVN Stack</span>
+      </v-toolbar-title>
+      <v-spacer />
+      <v-btn icon>
+        <v-icon>mdi-logout</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-main></v-main>
+    <v-btn v-scroll="onScroll" bottom color="red" dark fab fixed right @click="toTop">
+      <v-icon>mdi-chevron-up</v-icon>
+    </v-btn>
+  </v-app>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  props:{
+    source: String,
+  },
+  data: () => ({
+    mini: false
+  }),
+  methods:{
+    onScroll(e){
+      if(typeof window === 'undefined') return
+      
+      const top = window.pageYOffset || e.target.scrollTop || 0
+      this.fab = top > 20
+    },
+    toTop(){
+      this.$vuetify.goTo(0)
+    }
   }
 }
 </script>
